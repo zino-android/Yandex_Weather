e06ddc3c2df89b161a755581956b2a16a2f36c59
chichkanov <agchichkanov@gmail.com>
2017-07-09 21:15:07 +0300

    Added fragments, drawer, icon
    Added weather, about, settings fragments, code refactoring

---
+++ app/src/main/java/com/chichkanov/yandex_weather/WeatherFragment.java
@@ -0,0 +1,12 @@
+package com.chichkanov.yandex_weather;
+
+import android.support.v4.app.Fragment;
+
+public class WeatherFragment extends Fragment {
+
+    public static WeatherFragment newInstance(){
+        WeatherFragment weatherFragment = new WeatherFragment();
+        return weatherFragment;
+    }
+
+}
